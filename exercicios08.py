ed6d36f3affaf34e71b573ddeed6d243e6edfc1b
Ramon <dayvidr.santos@gmail.com>
2024-09-02 11:52:03 -0300

    atividades 02/09/2024

--- exercicios08.py
+++ exercicios08.py
@@ -1,9 +1,14 @@
 litros = float(input("Digite a quantidade de combustível: "))
-tipo = input("")
+tipo = input("Digite 'G' para Gasolina ou 'E' para Etanol: ")
 
-if tipo == "E" and tipo == "e":
-    valor = litros * 4.90
+if tipo == "E" or tipo == "e" or tipo == "G" or tipo == "g":
+    if tipo == "E" or tipo == "e":
+        valor = litros * 4.90
+    elif tipo == "G" or tipo == "g":
+        valor = litros * 5.80
+
     print(f"O valor a ser pago é de {valor}.")
+
 else:
-    valor = litros * 5.80
-    print(f"O valor a ser pago é de {valor}.")
+    print("Tipo de combustível é inválido")
+
