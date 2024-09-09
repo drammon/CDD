7125be36bd24494678874c9005d90bcface7384e
Ramon <dayvidr.santos@gmail.com>
2024-09-09 10:46:18 -0300

    atividades 09/09/2024

---
+++ exercicio12.py
@@ -0,0 +1,5 @@
+soma = 0
+for i in range (10):
+    num = int(input(f"Digite o {i+1} número: "))
+    soma += num
+print(soma)
---
+++ exercicio13.py
@@ -0,0 +1,4 @@
+num = int(input("Digite um número: "))
+for i in range(10):
+    mult = num * (i + 1)
+    print(f"{num} * {i + 1} = {mult}")
