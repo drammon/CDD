eb5743cf9090f66c295095c22fd0064933766fa7
Ramon <dayvidr.santos@gmail.com>
2024-09-11 11:43:52 -0300

    atividades 09/09/2024

--- exercicio19.py
+++ exercicio19.py
@@ -1,11 +1,13 @@
-num1 = int(input("Digite o primeiro número: "))
-num2 = int(input("Digite o segundo número: "))
+resp = 0
+while resp != 2:
+    num1 = int(input("Digite o primeiro número: "))
+    num2 = int(input("Digite o segundo número: "))
 
-while num2 == 0:
-    num = int(input("O número não pode ser 0, digite outro: "))
-    num2 = num
+    while num2 == 0:
+        num2 = int(input("O número não pode ser 0, digite outro: "))
 
-divisao = num1/num2
+    divisao = num1/num2
 
-print(divisao)
+    print(divisao)
 
+    resp = int(input("Quer repetir a operação? Digite 1 para sim e 2 para não: "))
---
+++ exercicio20.py
@@ -0,0 +1,10 @@
+negativos = 0
+
+for i in range(10):
+    num = int(input("Digite um número: "))
+    if num <0:
+        negativos += 1
+1
+print(f"{negativos} dos 10 são negativos.")
+
+
